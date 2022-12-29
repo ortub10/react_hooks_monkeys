09b34c4b31e52413882019c70e3a52b3c3395c48
ortub10 <ortub10@gmail.com>
2022-12-29 12:16:34 +0200

    useState

--- hooks/src/components/counter.js
+++ hooks/src/components/counter.js
@@ -2,9 +2,19 @@ import { useState } from "react";
 
 function Counter(props) {
   let [counter, setCounter] = useState(5);
+  let [user, setUser] = useState("koko");
+
+  const addCounter = () => {
+    setCounter(counter + 1);
+  };
   return (
     <div>
-      <h2>Counter: {counter}</h2>
+      <h2>
+        Counter: {counter} - User: {user}
+      </h2>
+      <button onClick={() => setUser("biden")}>Change user</button>
+      <button onClick={() => setCounter(0)}>Reset counter</button>
+      <button onClick={addCounter}>Add counter</button>
     </div>
   );
 }
