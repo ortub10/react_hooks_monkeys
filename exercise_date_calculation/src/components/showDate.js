3e04a95ccc84950e0651ea15e49f9444432ed4c5
ortub10 <ortub10@gmail.com>
2022-12-29 20:15:14 +0200

    part 2 - calculating days

--- exercise_date_calculation/src/components/showDate.js
+++ exercise_date_calculation/src/components/showDate.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function ShowDate(props) {
+  let [days, setDays] = useState();
+
+  useEffect(() => {
+    let time = Date.parse(props.deadline) - Date.now();
+    let newDays = Math.floor(time / (1000 * 60 * 60 * 24));
+    setDays(newDays);
+  }, [props.deadline]);
   return (
     <div className="col-lg-6 text-center mx-auto">
       <h2>Date countdown: {props.deadline} </h2>
-      <h3>Days left: --</h3>
+      <h3>Days left: {days}</h3>
     </div>
   );
 }
