ea1e8d12212f587f8aab044a14b3cd1653dddd2a
ortub10 <ortub10@gmail.com>
2022-12-31 19:43:09 +0200

    work with router

---
+++ router/src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/home";
+import Gallery from "./components/gallery";
+import About from "./components/about";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header className="boder p-2 container">
+        <Link to="/" className="me-2">
+          home
+        </Link>
+        <Link to="/about" className="me-2">
+          about
+        </Link>
+        <Link to="/gallery" className="me-2">
+          gallery
+        </Link>
+      </header>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route
+          path="/*"
+          element={
+            <div>
+              <h2>Page not fount, 404!</h2>
+            </div>
+          }
+        />
+      </Routes>
+      <footer>
+        <h3>footer</h3>
+      </footer>
+    </BrowserRouter>
+  );
+}
+export default App;
