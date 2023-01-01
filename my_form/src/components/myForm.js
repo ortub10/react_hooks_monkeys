6a77d69e371e20d85e61b9b4ca3d0da44527e37c
ortub10 <ortub10@gmail.com>
2023-01-01 21:03:12 +0200

    correctness of the form - email and select

--- my_form/src/components/myForm.js
+++ my_form/src/components/myForm.js
@@ -4,11 +4,13 @@ import { useForm } from "react-hook-form";
 export default function MyForm() {
   const {
     register,
+    getValues,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
   const onSub = (bodyData) => {
+    delete bodyData.email2;
     console.log(bodyData);
   };
   return (
@@ -36,6 +38,44 @@ export default function MyForm() {
           <div className="text-danger d-block">
             Enter valid phone - minimun 6 chars
           </div>
+        )}
+
+        <label>Email:</label>
+        <input
+          {...register("email", {
+            required: true,
+            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+          })}
+          type="text"
+          className="form-control"
+        />
+        {errors.email && (
+          <div className="text-danger d-block">Enter valid email</div>
+        )}
+        <label>Email repeat:</label>
+        <input
+          {...register("email2", {
+            required: true,
+            validate: (value) => value === getValues("email"),
+          })}
+          type="text"
+          className="form-control"
+        />
+        {errors.email2 && (
+          <div className="text-danger d-block">Email not equal</div>
+        )}
+        <label>Select color: </label>
+        <select
+          {...register("colors", { required: true })}
+          className="form-select"
+        >
+          <option value="">Choose color</option>
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+          <option value="green">Green</option>
+        </select>
+        {errors.colors && (
+          <div className="text-danger d-block">Choose color please</div>
         )}
         <button className="btn btn-info mt-3">Order</button>
       </form>
